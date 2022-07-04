dcbadb7f71dd2ca0a2e9bab106a4605f7eb9e037
fs2016g <arcturussoftware.io>
2022-07-03 20:17:30 -0600

    Another day, another push

---
+++ WebAPIframework4-6-2/Controllers/PeopleController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIframework4_6_2.Models;
+
+namespace WebAPIframework4_6_2.Controllers
+{
+    public class PeopleController : ApiController
+    {
+
+        List<Person> people = new List<Person>();
+        public PeopleController()
+        {
+
+            people.Add(new Person { FirstName = "John", LastName = "Doe", Id = 1 });
+            people.Add(new Person { FirstName = "Tom", LastName = "Harrison", Id = 2 });
+            people.Add(new Person { FirstName = "Jill", LastName = "Jackson", Id = 3 });
+
+        }
+        // GET: api/People
+        public List<Person> Get()
+        {
+            return people;
+        }
+
+        // GET: api/People/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/People
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // DELETE: api/People/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
