a437f1d63299345fe8490956f9c52af9b2fe183f
fs2016g <arcturussoftware.io>
2022-07-04 01:17:09 -0600

    this is now a shell for future use with Gh API for TakeHome

--- WebAPIframework4-6-2/Controllers/PeopleController.cs
+++ WebAPIframework4-6-2/Controllers/PeopleController.cs
@@ -8,6 +8,9 @@ using WebAPIframework4_6_2.Models;
 
 namespace WebAPIframework4_6_2.Controllers
 {
+    /// <summary>
+    /// This is where I give you all the info about my people.
+    /// </summary>
     public class PeopleController : ApiController
     {
 
@@ -20,6 +23,28 @@ namespace WebAPIframework4_6_2.Controllers
             people.Add(new Person { FirstName = "Jill", LastName = "Jackson", Id = 3 });
 
         }
+
+        /// <summary>
+        /// Gets a list of the first name of all users
+        /// </summary>
+        /// <param name="userId">The unique identifier for this person</param>
+        /// <param name="age">We want to get the age as well</param>
+        /// <returns>A list of first names, probably not the age and userId though</returns>
+
+        [Route("api/People/GetFirstNames/{userId:int}/{age:int}")]
+        [HttpGet]
+        public List<string> GetFirstNames(int userId, int age)
+        {
+            List<string> output = new List<string>();
+
+            foreach (var p in people)
+            {
+                output.Add(p.FirstName);
+            }
+
+            return output;
+        }
+
         // GET: api/People
         public List<Person> Get()
         {
--- WebAPIframework4-6-2/Models/Person.cs
+++ WebAPIframework4-6-2/Models/Person.cs
@@ -5,10 +5,24 @@ using System.Web;
 
 namespace WebAPIframework4_6_2.Models
 {
+    /// <summary>
+    /// Represents one specific person.
+    /// </summary>
     public class Person
     {
+        /// <summary>
+        /// ID from SQL
+        /// </summary>
         public int Id { get; set; } = 0;
+
+        /// <summary>
+        /// User's first name
+        /// </summary>
         public string FirstName { get; set; } = "";
+
+        /// <summary>
+        /// I'm sure you can guess
+        /// </summary>
         public string LastName { get; set; } = "";
     }
 }
